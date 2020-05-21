2670391fba880e2744219dacc41ea14af8b172f7
Ryan Johnston <ryanj@ryanj.org>
2020-05-21 16:48:47 -0600

    Initial commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,14 @@
+const Token = artifacts.require("Token");
+const EthSwap = artifacts.require("EthSwap");
+
+module.exports = async function(deployer) {
+  // Deploy Token
+  await deployer.deploy(Token);
+  const token = await Token.deployed()
+
+  // Deploy EthSwap
+  await deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed()
+
+  await token.transfer(ethSwap.address, '1000000000000000000000000')
+};
